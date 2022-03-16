dc3b3b23cb40924ec277a1ba542cc2dbd052372f
qipchaq <nurkali.adil@gmail.com>
2022-03-16 22:41:03 +0100

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng
+            },
+            headers: {
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '468fc19c24mshc8c3e9dd7edc1bdp186eaajsn078f692aaf9b'
+            }
+        })
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from "@material-ui/core";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Rating from "@material-ui/lab/Rating"
+
+import useStyles from "./styles"
+
+const PlaceDetails = ({ place }) => {
+    return (
+        <h1>{place.name}</h1>
+    )
+}
+
+export default PlaceDetails
